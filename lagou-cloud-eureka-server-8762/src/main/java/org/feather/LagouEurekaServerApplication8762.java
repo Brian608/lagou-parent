9b017b23a77d87acea1e13190da78bf1d6ff49a7
feather <Brian608@163.com>
2021-12-17 23:32:33 +0800

    eureka HA

---
+++ lagou-cloud-eureka-server-8762/src/main/java/org/feather/LagouEurekaServerApplication8762.java
@@ -0,0 +1,19 @@
+package org.feather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @program: lagou-parent
+ * @description:
+ * @author: 杜雪松(feather)
+ * @since: 2021-12-17 23:29
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class LagouEurekaServerApplication8762 {
+    public static void main(String[] args) {
+        SpringApplication.run(LagouEurekaServerApplication8762.class,args);
+    }
+}
