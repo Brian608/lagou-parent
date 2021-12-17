a6767cf84a819c16d22b3beafdf22ec5614903da
feather <Brian608@163.com>
2021-12-17 23:17:32 +0800

    eureka搭建

---
+++ lagou-cloud-eureka-server-8761/src/main/java/org/feather/LagouEurekaServerApplication.java
@@ -0,0 +1,11 @@
+package org.feather;/**
+* @program: lagou-parent
+*
+* @description: 
+*
+* @author: 杜雪松(feather)
+*
+* @since: 2021-12-17 22:49
+**/
+public class LagouEurekaServerApplication {
+}
